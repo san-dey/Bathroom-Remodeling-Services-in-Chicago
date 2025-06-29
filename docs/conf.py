8d8dbc4e2ae8be2c5d1bdc30019691f4ff25d0ef
san-dey <najvimahil@gmail.com>
2025-06-29 20:21:31 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'Bathroom Remodeling Services in Chicago'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
